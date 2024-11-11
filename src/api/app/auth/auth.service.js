e527f3da4dbe2ae6564f43cec66cd46f12590ebd
Yevhenii Bielykh <zhekabely12@gmail.com>
2024-11-11 02:38:27 +0100

    Throwing exceptions have been changed with "next(err)" calls

--- src/api/app/auth/auth.service.js
+++ src/api/app/auth/auth.service.js
@@ -3,14 +3,14 @@ import { UserModel } from '../../../utils/schemas/user.schema.js';
 import { CryptoService } from '../../../utils/services/crypto.service.js';
 import { userModelToResponseDto } from './mappers.js';
 
-async function login(req, res) {
+async function login(req, res, next) {
   const { login, password } = req.body;
   const user = await UserModel.findOne({ login });
 
-  if (!user) throw new UnauthorizedException();
+  if (!user) return next(new UnauthorizedException());
 
   const isPwdValid = await CryptoService.hasEqualPasswords(user, password);
-  if (!isPwdValid) throw new UnauthorizedException();
+  if (!isPwdValid) return next(new UnauthorizedException());
 
   req.session.user = userModelToResponseDto(user);
   res.json(req.session.user);
--- src/utils/exceptions/finish-validation.js
+++ src/utils/exceptions/finish-validation.js
@@ -4,5 +4,5 @@ export function finishValidation(errors, next) {
   if (!Object.values(errors).some(arr => arr.length)) return next();
 
   Object.entries(errors).forEach(([key, value]) => !value.length && (errors[key] = undefined));
-  throw new BadRequestException('Login falied', errors);
+  next(new BadRequestException('Login falied', errors));
 }
